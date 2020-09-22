5bc09c4fb1b3d45219515d5f5c09b1d071f87744
surajmohite30 <surajmohite3094@gmail.com>
2020-09-22 12:07:17 +0530

    tomcat files

--- src/main/java/monitoring/app/ActuatorApplication.java
+++ src/main/java/monitoring/app/ActuatorApplication.java
@@ -2,11 +2,12 @@ package monitoring.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @SpringBootApplication
-public class ActuatorApplication {
+public class ActuatorApplication extends SpringBootServletInitializer {
 	@CrossOrigin(origins="http://localhost:4200")
 	public static void main(String[] args) {
 		SpringApplication.run(ActuatorApplication.class, args);
